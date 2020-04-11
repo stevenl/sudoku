cf5e1929d9485e8320bf8883e3bb333ea313a7cd
Steven Lee <stevenwh.lee@gmail.com>
2020-04-11 18:17:27 +0200

    Keep track of the wanted values in a CellGroup

--- src/main/java/io/stevenl/sudoku/CellGroup.java
+++ src/main/java/io/stevenl/sudoku/CellGroup.java
@@ -1,14 +1,28 @@
 package io.stevenl.sudoku;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class CellGroup {
     private Cell[] cells;
+    private Set<Integer> wantedValues;
 
     public CellGroup(int size) {
         cells = new Cell[size];
+
+        wantedValues = new HashSet<>(size);
+        for (int i = 1; i <= size; i++) {
+            wantedValues.add(i);
+        }
     }
 
     public void addCell(int index, Cell cell) {
         cells[index] = cell;
+
+        int value = cell.getValue();
+        if (value > 0) {
+            wantedValues.remove(value);
+        }
     }
 
     public Cell[] getCells() {
