75ac79255d910582b43eef8edf85195d960b2e1b
Steven Lee <stevenwh.lee@gmail.com>
2020-05-05 09:35:29 +0200

    Add more levels to the webapp

--- src/main/java/io/stevenl/sudoku/PuzzleController.java
+++ src/main/java/io/stevenl/sudoku/PuzzleController.java
@@ -11,14 +11,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PuzzleController {
     private static final Logger LOGGER = LoggerFactory.getLogger(PuzzleController.class);
 
     @GetMapping("/puzzles")
-    public String getPuzzle(Model model) throws SudokuException {
-        Grid grid = new Grid("000000080005073090000900300000200709900136004403009000001005000060840900070000000");
+    public String getPuzzle(@RequestParam(name = "level") String level, Model model) throws SudokuException {
+        Grid grid = getGrid(level);
         model.addAttribute("grid", grid);
 
         Solver solver = new Solver(grid);
@@ -37,5 +38,26 @@ public class PuzzleController {
         LOGGER.info("solvable = {}", solver.getSolvableCells());
 
         return "puzzle";
+    }
+
+    private Grid getGrid(String level) throws SudokuException {
+        String puzzle;
+        switch (level) {
+            case "easy":
+                puzzle = "000483276600102580020000100006007000130809047000600900008000060057201008469578000";
+                break;
+            case "medium":
+                puzzle = "070001000005009003103074000608000030901000207020000908000950602400300500000700080";
+                break;
+            case "hard":
+                puzzle = "000090008000000010413706002004900003090040050600008400800509741020000000500010000";
+                break;
+            case "evil":
+                puzzle = "000000080005073090000900300000200709900136004403009000001005000060840900070000000";
+                break;
+            default:
+                throw new SudokuException("Unrecognised level");
+        }
+        return new Grid(puzzle);
     }
 }
