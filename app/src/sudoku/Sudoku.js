2c45d9a4daabb5bc6340d61e007f9511f17e41c6
Steven Lee <stevenwh.lee@gmail.com>
2020-10-03 22:07:23 +0200

    Use useContext and useReducer hooks
    Use reducer() to encapsulate the logic for updating state.
    
    Use context to pass dispatch() to Cell components directly instead of passing callbacks through the component tree.

--- app/src/sudoku/Sudoku.js
+++ app/src/sudoku/Sudoku.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useContext, useReducer } from 'react';
 import './Sudoku.css';
 
 const GRID_SIZE = 9;
@@ -6,8 +6,23 @@ const GRID_SIZE = 9;
 const CELL_RANGE = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 const ROW_LABELS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'];
 
+// noinspection JSUnusedLocalSymbols
+const GridDispatch = React.createContext(action => null);
+
+function reducer(grid, action) {
+    const cellIdx = action.index;
+    if (grid[cellIdx].readOnly) {
+        return grid;
+    }
+
+    const newGrid = grid.slice();
+    newGrid[cellIdx] = {value: action.value};
+    return newGrid;
+}
+
 function Sudoku(props) {
-    const [grid, setGrid] = useState(parseGrid(props.gridString));
+    const initialGrid = parseGrid(props.gridString);
+    const [grid, dispatch] = useReducer(reducer, initialGrid);
 
     function parseGrid(gridString) {
         const values = (gridString).split('');
@@ -24,62 +39,50 @@ function Sudoku(props) {
     }
 
     return (
-        <div className="sudoku">
-            <table>
-                <caption>Sudoku</caption>
-
-                <colgroup>
-                    <col />
-                </colgroup>
-                <colgroup className="region">
-                    <col span="3" />
-                </colgroup>
-                <colgroup className="region">
-                    <col span="3" />
-                </colgroup>
-                <colgroup className="region">
-                    <col span="3" />
-                </colgroup>
-
-                <HeaderRow />
-                <tbody className="region">
-                    {renderRow(0)}
-                    {renderRow(1)}
-                    {renderRow(2)}
-                </tbody>
-                <tbody className="region">
-                    {renderRow(3)}
-                    {renderRow(4)}
-                    {renderRow(5)}
-                </tbody>
-                <tbody className="region">
-                    {renderRow(6)}
-                    {renderRow(7)}
-                    {renderRow(8)}
-                </tbody>
-            </table>
-        </div>
+        <GridDispatch.Provider value={dispatch}>
+            <div className="sudoku">
+                <Grid grid={grid} />
+            </div>
+        </GridDispatch.Provider>
     );
+}
 
-    function renderRow(row) {
-        return (
-            <Row
-                row={row}
-                grid={grid}
-                onChange={(idx, val) => handleCellChange(idx, val)}
-            />
-        );
-    }
+function Grid(props) {
+    return (
+        <table>
+            <caption>Sudoku</caption>
 
-    function handleCellChange(cellIdx, value) {
-        if (grid[cellIdx].readOnly) {
-            return;
-        }
+            <colgroup>
+                <col />
+            </colgroup>
+            <colgroup className="region">
+                <col span="3" />
+            </colgroup>
+            <colgroup className="region">
+                <col span="3" />
+            </colgroup>
+            <colgroup className="region">
+                <col span="3" />
+            </colgroup>
 
-        const newGrid = grid.slice();
-        newGrid[cellIdx] = {value: value};
-        setGrid(newGrid);
-    }
+            <HeaderRow />
+            <tbody className="region">
+                <Row row={0} grid={props.grid} />
+                <Row row={1} grid={props.grid} />
+                <Row row={2} grid={props.grid} />
+            </tbody>
+            <tbody className="region">
+                <Row row={3} grid={props.grid} />
+                <Row row={4} grid={props.grid} />
+                <Row row={5} grid={props.grid} />
+            </tbody>
+            <tbody className="region">
+                <Row row={6} grid={props.grid} />
+                <Row row={7} grid={props.grid} />
+                <Row row={8} grid={props.grid} />
+            </tbody>
+        </table>
+    );
 }
 
 function HeaderRow() {
@@ -107,7 +110,6 @@ function Row(props) {
                     key={cellIdx}
                     index={cellIdx}
                     cell={props.grid[cellIdx]}
-                    onChange={props.onChange}
                 />;
             })}
         </tr>
@@ -116,13 +118,17 @@ function Row(props) {
 
 function Cell(props) {
     const cell = props.cell;
+    const dispatch = useContext(GridDispatch);
     return (
         <td>
             <input
                 type="number" min="1" max="9"
                 value={cell.value ? cell.value : ''}
                 readOnly={cell.readOnly}
-                onChange={(e) => props.onChange(props.index, e.target.valueAsNumber)}
+                onChange={(event) => dispatch({
+                    index: props.index,
+                    value: event.target.valueAsNumber,
+                })}
             />
         </td>
     );
